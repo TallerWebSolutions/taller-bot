442533aa5e8a8417847e0c83d60c8c19f51d6338
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2018-11-13 22:34:02 -0200

    Added serving script

---
+++ scripts/serve.js
@@ -0,0 +1,9 @@
+require('dotenv').config()
+const { spawn } = require('child_process');
+
+if (!process.env.PORT) {
+  console.error('You must define a PORT environment variable before executing "serve".')
+  process.exit(1)
+}
+
+spawn(`ngrok`, ['http', process.env.PORT], { stdio: 'inherit' })
