d3978918f50e3a7e495c2d81db7e81f4b51d6361
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2018-11-29 23:26:58 -0200

    feat: implemented /choose command

--- src/modules/choose.js
+++ src/modules/choose.js
@@ -1,7 +1,50 @@
+function shuffle (array) {
+  let i = 0
+  let j = 0
+  let temp = null
+
+  for (i = array.length - 1; i > 0; i -= 1) {
+    j = Math.floor(Math.random() * (i + 1))
+    temp = array[i]
+    array[i] = array[j]
+    array[j] = temp
+  }
+}
+
+const toTextualList = items =>
+  items.map((item, i, arr) =>
+    i > 0 ? (i === arr.length - 1 ? ' and ' : ', ') + item : item
+  ).join('')
+
 export default {
+  init: (server, controller) => {
+    controller.hears('foobar', '', (bot, message) => {
+      bot.reply(message, 'Caraca')
+    })
+  },
+
   slash: {
-    choose: (command, message) => {
-      command.replyPrivate(message, 'There we go!')
+    choose: (bot, message) => {
+      let [amount, list] = message.text.split('from')
+
+      list = list === undefined ? amount : list
+      amount = parseInt(amount) || 1
+
+      const plural = amount > 1
+
+      const items = list
+        .split(/,|[, ]e |[, ]ou |[, ]or /)
+        .map(text => text.trim())
+        .filter(Boolean)
+
+      shuffle(items)
+
+      bot.replyPublic(
+        message,
+        `Here ${plural ? 'are' : 'is'} my choice${
+          plural ? 's' : ''
+        }: ${toTextualList(items.slice(0, amount))}`
+      )
     }
   }
 }
